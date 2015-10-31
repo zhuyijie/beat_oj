b517969c2ef626580aa6f97dc92f5c20a506ad37
zhuyijie <yijiezhu2@gmail.com>
2015-10-31 14:31:11 +0800

    1002

--- src/com/baidu/Main1002.java
+++ src/com/baidu/Main1002.java
@@ -1,72 +1,110 @@
 package com.baidu;
 
-import java.util.ArrayList;
-import java.util.Collections;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.io.PrintWriter;
+import java.util.Arrays;
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
-import java.util.Scanner;
 
 public class Main1002 {
 
-    private static final Map<Character, Integer> char2Int;
+    private static final Map<Character, Character> char2Num;
 
     static {
         Object[][] maps = new Object[][] {
-                {1, "ij"}, {2, "abc"},
-                {3, "def"}, {4, "gh"},
-                {5, "kl"}, {6, "mn"},
-                {7, "prs"}, {8, "tuv"},
-                {9, "wxy"}, {0, "oqz"}
+                {'1', "ij"}, {'2', "abc"},
+                {'3', "def"}, {'4', "gh"},
+                {'5', "kl"}, {'6', "mn"},
+                {'7', "prs"}, {'8', "tuv"},
+                {'9', "wxy"}, {'0', "oqz"}
         };
-        char2Int = new HashMap<>();
+        char2Num = new HashMap<>();
         for (Object[] map : maps) {
-            Integer num = (Integer) map[0];
+            Character num = (Character) map[0];
             String chars = (String) map[1];
             for (int i = 0; i < chars.length();i++) {
-                char2Int.put(chars.charAt(i), num);
+                char2Num.put(chars.charAt(i), num);
             }
         }
     }
 
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
+    public static void main(String[] args) throws IOException {
+        BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+        PrintWriter out = new PrintWriter(new OutputStreamWriter(System.out));
         do {
-            String num = scanner.nextLine();
+            String num = in.readLine();
             if (num.charAt(0) == '-') {
                 break;
             }
-            int numOfWords = scanner.nextInt();
-            String[] words = new String[0];
+            int numOfWords = Integer.valueOf(in.readLine());
+            String[] words = new String[numOfWords];
+            String[] translated = new String[numOfWords];
             for (int i = 0;i < numOfWords;i++) {
-                words[i] = scanner.nextLine();
+                words[i] = in.readLine();
+                translated[i] = translate(words[i]);
+            }
+            String[] answer = dp(num, words, translated);
+            if (answer == null) {
+                out.println("No solution.");
+            } else {
+                int size = answer.length;
+                for (int i = 0;i < size;i++) {
+                    if (i != 0) {
+                        out.print(" ");
+                    }
+                    out.print(answer[i]);
+                }
+                out.println();
             }
         } while (true);
+        out.flush();
     }
 
-    public List<String> solve(String num, String[] words) {
-        List[] dict = new List[26];
-        for (String word : words) {
-            int index = word.charAt(0) - 'a';
-            if (dict[index] == null) {
-                dict[index] = new ArrayList<>();
-            }
-            dict[index].add(word);
+    public static String translate(String word) {
+        char[] nums = new char[word.length()];
+        for (int i = 0;i < word.length();i++) {
+            nums[i] = char2Num.get(word.charAt(i));
         }
-        return dfs(num, dict);
+        return new String(nums);
     }
 
-    public List<String> dfs(String num, List<String>[] dict) {
-        if (num.isEmpty()) {
-            return Collections.emptyList();
-        }
-        int index = num.charAt(0) - 'a';
-        if (dict[index] == null) return null;
-        
-        for (String candidate : dict[index]) {
-            if (num.startsWith(candidate)) {
 
+    /**
+     *
+     * dp the result, remember to reverse it
+     *
+     * */
+    public static String[] dp(String num, String[] dict, String[] translated) {
+        int[] optimal = new int[101];
+        Arrays.fill(optimal, Integer.MAX_VALUE - 1);
+        optimal[0] = 0;
+        String[] optimalValue = new String[101];
+
+        for(int i = 1;i <= num.length();i++) {
+            for (int j = 0;j < dict.length;j++) {
+                int l = translated[j].length();
+                if (l <= i && optimal[i - l] + 1 < optimal[i] && num.substring(i - l, i).equals(translated[j])) {
+                    optimal[i] = optimal[i - l] + 1;
+                    optimalValue[i] = dict[j];
+                }
             }
+        }
+        if (optimal[num.length()] == Integer.MAX_VALUE - 1) {
+            return null;
+        } else {
+            int resultSize = optimal[num.length()];
+            String[] result = new String[resultSize];
+            int pivot = num.length();
+            int resultPivot = resultSize - 1;
+            while (optimal[pivot] > 0 && resultPivot >= 0) {
+                result[resultPivot] = optimalValue[pivot];
+                pivot = pivot - optimalValue[pivot].length();
+                resultPivot--;
+            }
+            return result;
         }
     }
 }
